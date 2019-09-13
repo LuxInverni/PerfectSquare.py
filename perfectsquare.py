816da27165258fd113f4fc5a7a80d39f0fdec8a1
LuxInverni <41523119+LuxInverni@users.noreply.github.com>
2019-09-13 15:24:19 +0100

    Create perfectsquare.py

---
+++ perfectsquare.py
@@ -0,0 +1,16 @@
+def perfectsquare(list):
+    duplicate = []
+    for i in list1:
+        sqroot = i**.5
+        if sqroot % 1 == 0:
+            print(f"{i} from List1 is a Perfect square as: {sqroot} * {sqroot} = {i}")
+            duplicate.append(i)
+    if len(duplicate) == 0:
+        print("No Perfect Squares In The List")
+    else:
+        print(f"The Perfect Squares in the List are: {duplicate}")
+    return
+    
+    # Call Function to go through a list and discover any perfect squares
+    # Outputs a list (duplicate) of the perfect squares found in any list
+    # If No Perfect Squares, The Function returns that there are no squares in list
